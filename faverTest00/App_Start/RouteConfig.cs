76b120f8ca45d52a1456126477319239b5535a95
deltagare <deltagare@LEXSMAAF12>
2017-03-02 15:34:35 +0100

    Final Check Before Submitting

--- faverTest00/App_Start/RouteConfig.cs
+++ faverTest00/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace faverTest00
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Fever", id = UrlParameter.Optional }
             );
         }
     }
---
+++ faverTest00/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace faverTest00.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Fever(int? tem)
+
+        {
+            if (tem <= 32)
+            {
+                ViewBag.msg = "You ar not Ok ";
+
+            }
+            else if (tem >= 38)
+            {
+                ViewBag.msg = "You have a Fever , go seek some ICE  ";
+            }
+
+            else if (tem >= 32 || tem <= 38)
+            {
+                ViewBag.msg = "You ar Ok "; 
+            }
+
+        
+            else
+            {
+                ViewBag.msg = " Enter the tem Value Pls  ";
+            }
+            return View();
+
+        }
+    }
+}
